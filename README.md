34c43603f04059fa010b40029dc05972fb909c4d
Daniel Saakes <daniel@saakes.net>
2025-10-08 09:17:02 +0200

    first

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # pose2p5
+
+
+# install
+
+
+## download models into the models folder
+
+https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_heavy/float16/1/pose_landmarker_heavy.task
+
+
---
+++ main.py
@@ -0,0 +1,76 @@
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+
+import numpy as np
+import cv2 as cv
+
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+  pose_landmarks_list = detection_result.pose_landmarks
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected poses to visualize.
+  for idx in range(len(pose_landmarks_list)):
+    pose_landmarks = pose_landmarks_list[idx]
+
+    # Draw the pose landmarks.
+    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    pose_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+    ])
+    solutions.drawing_utils.draw_landmarks(
+      annotated_image,
+      pose_landmarks_proto,
+      solutions.pose.POSE_CONNECTIONS,
+      solutions.drawing_styles.get_default_pose_landmarks_style())
+  return annotated_image
+
+
+
+def main():
+    base_options = python.BaseOptions(model_asset_path='models/pose_landmarker_heavy.task')
+    options = vision.PoseLandmarkerOptions(
+        base_options=base_options,
+        output_segmentation_masks=True)
+    detector = vision.PoseLandmarker.create_from_options(options)
+
+    cap = cv.VideoCapture(0)
+    cap.set(cv.CAP_PROP_FRAME_WIDTH,640) 
+    cap.set(cv.CAP_PROP_FRAME_HEIGHT,480)
+
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+
+    while True:
+        ret, frame = cap.read()
+    
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        detection_result = detector.detect(mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb))
+
+
+        annotated_image = draw_landmarks_on_image(rgb, detection_result)
+
+        bgr = cv.cvtColor(annotated_image, cv.COLOR_RGB2BGR)
+
+        # Our operations on the frame come here
+        #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        # Display the resulting frame
+        cv.imshow('frame', bgr)
+
+        if cv.waitKey(1) == 27:
+            break
+    
+    # When everything done, release the capture
+    cap.release()
+    cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
